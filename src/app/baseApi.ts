0ca2b71d934ac13ce4f961729ef39edbcfb52300
Filipp Sam <philippsm666@gmail.com>
2025-07-27 23:29:20 +0300

    first commit

---
+++ src/app/baseApi.ts
@@ -0,0 +1,24 @@
+import { AUTH_TOKEN } from "@/common/constants"
+import { handleError } from "@/common/utils"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const baseApi = createApi({
+  reducerPath: "todolistsApi",
+  tagTypes: ["Todolist", "Task"],
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      prepareHeaders: (headers) => {
+        headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+        headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+      },
+    })(args, api, extraOptions)
+
+    handleError(api, result)
+
+    return result
+  },
+  endpoints: () => ({}),
+  keepUnusedDataFor: 1000, //время жизни в кеше
+  /* refetchOnFocus: true, //автоматического повторного запроса за данными, когда окно приложения или вкладка браузера возвращаются в фокус */
+})
